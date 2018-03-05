24362e256d59971be55ba6fca4dbe5ec93568437
Mateusz Gawino <mateusz.gawino@gmail.com>
2018-03-05 22:33:08 +0100

    Skeleton of web application

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import setuptools
 
 setuptools.setup(
-    name=’Cloud File Manager’,
-    version=’0.1.0’,
-    author=’Mateusz Gawino’,
-    packages=setuptools.find_packages(exclude=[‘tests’])
+    name='Cloud File Manager',
+    version='0.1.0',
+    author='Mateusz Gawino',
+    packages=setuptools.find_packages(exclude=['tests'])
 )
