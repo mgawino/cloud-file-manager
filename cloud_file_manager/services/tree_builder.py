3f9da91a0851b83fe1d6ae87161b9932b5897f2c
Mateusz Gawino <mateusz.gawino@gmail.com>
2018-03-18 19:03:01 +0100

    Renames in tree builder

--- cloud_file_manager/services/tree_builder.py
+++ cloud_file_manager/services/tree_builder.py
@@ -5,31 +5,31 @@ import os
 class JSTreeBuilder:
 
     @staticmethod
-    def _create_node(text, s3_key):
-        if s3_key == '':
+    def _create_node(node_name, path):
+        if path == '':
             type = 'bucket'
         else:
             type = 'folder'
         return {
-            'text': text,
+            'text': node_name,
             'type': type,
             'children': []
         }
 
-    def _add_node(self, tree, s3_key):
-        if s3_key not in tree:
-            node = self._create_node(text=os.path.basename(s3_key), s3_key=s3_key)
-            tree[s3_key] = node
-            splitted = s3_key.rsplit('/')
+    def _add_node(self, tree, path):
+        if path not in tree:
+            node = self._create_node(node_name=os.path.basename(path), path=path)
+            tree[path] = node
+            splitted = path.rsplit('/')
             parent_s3_key = '' if len(splitted) == 1 else splitted[0]
             parent_node = self._add_node(tree, parent_s3_key)
             parent_node['children'].append(node)
             return node
         else:
-            return tree[s3_key]
+            return tree[path]
 
-    def make_json_tree(self, bucket_name, bucket_keys):
-        tree = {'': self._create_node(text=bucket_name, s3_key='')}
-        for s3_key in bucket_keys:
+    def make_json_tree(self, root_name, paths):
+        tree = {'': self._create_node(node_name=root_name, path='')}
+        for s3_key in paths:
             self._add_node(tree, s3_key)
         return tree['']
