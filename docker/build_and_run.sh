860ae9d2e814c880fa8dc74b07d4e39f43706685
Mateusz Gawino <mateusz.gawino@gmail.com>
2018-03-18 21:31:43 +0100

    Fix docker options

--- docker/build_and_run.sh
+++ docker/build_and_run.sh
@@ -2,7 +2,8 @@ docker build -t cloud-file-manager --no-cache .
 docker run -d \
            -e AWS_ACCESS_KEY_ID=key \
            -e AWS_SECRET_ACCESS_KEY=secret \
-           -e S3_ENDPOINT_URL=http://localhost:4572 \
+           -e S3_ENDPOINT_URL=http://0.0.0.0:4572 \
            -e APP_PORT=5000 \
+           --net=host \
            -p 5000:5000 \
            cloud-file-manager
